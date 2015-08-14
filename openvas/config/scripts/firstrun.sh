29a73e4d261eb3ded505dacec2544e826eaf1568
ccRidge <cl.mail.forwarding@gmail.com>
2015-08-13 22:46:41 -0700

    Update firstrun.sh

--- openvas/config/scripts/firstrun.sh
+++ openvas/config/scripts/firstrun.sh
@@ -2,11 +2,22 @@
 
 mkdir -p /var/lib/openvas/private/CA /var/lib/openvas/CA /var/lib/openvas/scap-data/private
 
-openvas-nvt-sync 
-openvas-scapdata-sync
-openvas-certdata-sync
+test -e /var/lib/openvas/CA/cacert.pem  || openvas-mkcert -q
+openvas-nvt-sync
+#openvas-scapdata-sync
+#openvas-certdata-sync
 
-service openvas-scanner restart
-service openvas-manager restart
-openvas-mkcert-client -n om -i
+test -e /var/lib/openvas/users/om || openvas-mkcert-client -n om -i
+service openvas-scanner stop
+service openvas-manager stop
+service openvas-gsa stop
+
+openvassd
+openvasmd --migrate
 openvasmd --rebuild --progress
+killall openvassd
+sleep 15
+service openvas-scanner start
+service openvas-manager start
+service openvas-gsa start
+
